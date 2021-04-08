8ec7b6e4d0eb3306236088e1608d51bbe33060d6
Víctor Peña Romero <vic.pero@icloud.com>
2021-04-08 15:32:34 -0500

    adjustments to basic lambda

--- backend/src/lambda/http/createBook.ts
+++ backend/src/lambda/http/createBook.ts
@@ -1,7 +1,31 @@
 export const handler = async (event) => {
 
-  console.log(event)
+  console.log(event.body)
 
-  return 'Lambda working'
+  try {
+    
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        message: 'Lambda working correctly!'
+      })
+    }
+
+  } catch (error) {
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        message: 'Lambda fail.'
+      })
+    }
+
+  }
 
 }
